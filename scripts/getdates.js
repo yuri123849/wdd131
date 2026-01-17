7a75180ba3152c95af58f636f1abf00d2401861c
Lucky Girl <xchin@byupathway.edu>
2026-01-17 09:40:36 +0800

    File changes

--- scripts/getdates.js
+++
@@ -1,7 +0,0 @@
-
-const year = document.querySelector("#currentyear");
-const today = new Date();
-year.innerHTML = today.getFullYear();
-
-
-document.getElementById("lastModified").innerHTML = document.lastModified;
